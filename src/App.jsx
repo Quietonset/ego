7b944637168993093e8c8fc87a27f432638f2f87
Quietonset <davidakachi9@gmail.com>
2024-11-29 18:09:11 +0100

    update on landing page on Ego website

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import { createHashRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 import Root from './layout/Root';
 import LandingPage from './pages/LandingPage';
 import "@fontsource/space-grotesk";
 
 const App = () => {
-  const router = createBrowserRouter(
+  const router = createHashRouter(
     createRoutesFromElements(
       <Route path='/' element={ <Root/> }>
         <Route index element={ <LandingPage/> }/>
